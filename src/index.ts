e9c34cfdb976feab80f58775af843ad43785e33a
Simon - fantomitechno <simon@renoux.dev>
2024-08-30 01:48:20 +0200

    ✨ Server 80% complete

--- src/index.ts
+++ src/index.ts
@@ -6,16 +6,26 @@ import EventEmitter from "events";
 import { config } from "dotenv";
 config();
 
+interface Status {
+  done: boolean;
+  failed: boolean;
+}
+
 declare interface InternalEvent {
-  on(event: "message", listener: (data: any) => void): this;
+  on(event: "message", listener: (data: Status) => void): this;
   on(event: "whitelist", listener: (username: string) => void): this;
-  emit(event: "message", data: any): boolean;
+  emit(event: "message", data: Status): boolean;
   emit(event: "whitelist", username: string): boolean;
+  removeListener(event: "message", listener: (data: Status) => void): this;
+  removeListener(
+    event: "whitelist",
+    listener: (username: string) => void
+  ): this;
 }
 class InternalEvent extends EventEmitter {}
 
 const app = new Hono();
-const { upgradeWebSocket } = createNodeWebSocket({ app });
+const { upgradeWebSocket, injectWebSocket } = createNodeWebSocket({ app });
 
 const internal = new InternalEvent();
 
@@ -31,6 +41,7 @@ app.post("/whitelist", async (c) => {
   const data: { username: string } = await c.req.json();
 
   const username = data["username"];
+  console.log("Whitelisting " + username);
 
   internal.emit("whitelist", username);
   return c.json({});
@@ -40,27 +51,35 @@ app.get(
   "/ws",
   upgradeWebSocket((c) => {
     const type = c.req.queries("type");
-    if (!type || type.length != 1) {
-      return {};
-    }
+    let messageReceiver: (data: Status) => void;
+    let whitelistReceiver: (username: string) => void;
     return {
       onMessage(event, _) {
-        if (type[0] == "server") {
-          internal.emit("message", event.data);
+        if (type && type[0] == "server") {
+          internal.emit("message", JSON.parse(event.data as string));
         }
       },
       onClose(event, ws) {
         console.log("Connection closed");
+        if (messageReceiver)
+          internal.removeListener("message", messageReceiver);
+        if (whitelistReceiver)
+          internal.removeListener("whitelist", whitelistReceiver);
       },
       onOpen(_, ws) {
-        if (type[0] !== "server") {
-          internal.on("whitelist", (username) => {
+        if (type && type[0] === "server") {
+          console.log("Server connected");
+          whitelistReceiver = (username) => {
             ws.send(username);
-          });
+          };
+          internal.on("whitelist", whitelistReceiver);
         } else {
-          internal.on("message", (data) => {
-            ws.send(data);
-          });
+          console.log("Client connected");
+          messageReceiver = (data) => {
+            ws.send(JSON.stringify(data));
+            console.log(data);
+          };
+          internal.on("message", messageReceiver);
         }
       },
     };
@@ -70,7 +89,9 @@ app.get(
 const port = Number(process.env.PORT);
 console.log(`Server is running on port ${port}`);
 
+injectWebSocket(
 serve({
   fetch: app.fetch,
   port,
-});
+  })
+);
